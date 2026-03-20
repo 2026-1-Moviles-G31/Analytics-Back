e6c6c0db0b654054cd375c8ffe27a162518c5877
mreinaroca <m.reina@uniandes.edu.co>
2026-03-20 17:19:29 -0500

    Bugfix with PostgreSQL

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./analytics.db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./analytics.db")
 
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
