213d6b8680fef9ef5f8a2040e118d0d4452aa3f2
Miguel Arturo Reina <98564328+mreinaroca@users.noreply.github.com>
2026-03-20 22:51:28 -0500

    Develop (#4)
    Fixed with CORSMiddleware and Python version

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from routes.analytics import router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Analytics Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 
